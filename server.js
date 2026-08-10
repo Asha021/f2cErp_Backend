b3ad7ca366edc9a62275703c97d2d4e78ab8efba
Asha021 <apal66058@gmail.com>
2026-08-10 17:29:10 +0530

    Fix duplicated code block in server.js

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ uploadDirs.forEach(dir => {
   }
 });
 
-const fs = require('fs');
-const uploadDirs = ['uploads', 'uploads/items', 'uploads/templates', 'uploads/invoices', 'uploads/pos', 'uploads/reports'];
-uploadDirs.forEach(dir => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-});
 
 const authRoutes = require('./routes/auth.routes');
 const companiesRoutes = require('./routes/companies.routes');
